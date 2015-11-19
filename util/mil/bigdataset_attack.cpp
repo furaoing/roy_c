0976373f30c55cefc990615d9bce50b6f646ccb7
furaoing <furaoing@hotmail.com>
2015-11-20 01:56:33 +0800

    added some new header files

---
+++ util/mil/bigdataset_attack.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <typeinfo>
+#include <random>
+#include "true_random.h"
+
+using namespace std;
+
+void generate_bigDataSet()
+{
+  ofstream myfile;
+  myfile.open ("example.txt");
+  int r_min = 0;
+  int r_max = int (pow(10.0, 9.0));
+  for(int i=0; i<0.5*r_max;i++)
+  {
+
+      myfile << true_random(r_min,r_max) << "\n";
+  }
+  myfile.close();
+
+}
+
+int collision_detection()
+{
+   int i;
+   int j;
+   char buffer[300];
+   int guess;
+   string str_buffer;
+   int int_buffer;
+   int round = 0;
+
+   int r_min = 0;
+   int r_max = int (pow(10.0, 9.0));
+
+   ifstream myfile("example.txt");
+
+    while(true)
+    {
+      round += 1;
+      cout << "Round " << round << " Processing...\n";
+      ifstream myfile("example.txt");
+       if(myfile.is_open())
+       {
+         guess = true_random(r_min, r_max);    //pick a guess;
+         for(i=0; i<0.5*r_max;i++)
+         {
+            myfile.getline(buffer,300);
+            str_buffer = {buffer};
+            int_buffer = stoi(str_buffer);
+            if(guess == int_buffer)
+            {
+                cout << "Inloop Fail" << guess << "\n";
+                break;
+            }
+
+            if(i == (0.5*r_max-1))
+            {
+                cout << guess;
+                return guess;
+            }
+         }
+       }
+       else
+         cout << "Unable to open file";
+       myfile.close();
+    }
+   return 0;
+}
+
+int main()
+{
+    generate_bigDataSet();
+    //collision_detection();
+}
---
+++ util/mil/insertion_sort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int insertion_sort(int *nums, int size)
+{
+    int n = size;
+    int i;
+    int key;
+    int j;
+    for(i = 2; i < n; i++)
+    {
+        key = *(nums + i);
+        j = i - 1;
+        while(j > 0 && *(nums + j) > key)
+        {
+            *(nums + j + 1) = *(nums + j);
+                    j = j - 1;
+        }
+        *(nums + j + 1) = key;
+    }
+
+    int head = *nums;
+    int k = 1;
+    while(*(nums+k) < head)
+    {
+        *(nums + k - 1) = *(nums + k);
+        k = k + 1;
+    }
+    *(nums + k - 1) = head;
+
+    return 1;
+}
+
+int main()
+{
+    int my_array[] = {5,3,2,4,1,77,8,33,31,3,4};
+    int size = sizeof(my_array)/sizeof(my_array[0]);
+    int result = insertion_sort(my_array, size);
+
+    int i;
+    for(i = 0; i < sizeof(my_array)/sizeof(int); i++)
+    {
+        printf("%d ", my_array[i]);
+    }
+
+    return 1;
+}
